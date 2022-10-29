02d04b13bc56f40ff9ae3878533eda6e086a0687
James Xalis <jamesxalis@icloud.com>
2022-10-28 23:15:27 -0700

    Finished adding models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category,{
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product,{
+foreignKey: 'category_id',
+onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag,{
+foreignKey: 'category_id',
+onDelete: 'CASCADE',
+});
+
+Tag.belongsToMany(Product,{
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
 module.exports = {
     Product,
